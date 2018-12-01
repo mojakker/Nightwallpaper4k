a9c1fa4bab5160cedea1b2d808f50b0bc320cadc
MOJAKKER <mojakkerhossain3@gmail.com>
2018-12-02 02:19:31 +0530

    nbsfb vlj

--- src/main/java/tech/wallpaperandwallaper/nightwallpaper4k/adapter/recycler_image_adapter.java
+++ src/main/java/tech/wallpaperandwallaper/nightwallpaper4k/adapter/recycler_image_adapter.java
@@ -1,30 +1,37 @@
 package tech.wallpaperandwallaper.nightwallpaper4k.adapter;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
 
 import tech.wallpaperandwallaper.nightwallpaper4k.R;
+import tech.wallpaperandwallaper.nightwallpaper4k.image_activity;
 import tech.wallpaperandwallaper.nightwallpaper4k.recyler_items.item_image;
+import tech.wallpaperandwallaper.nightwallpaper4k.tools.static_tools;
 
 public class recycler_image_adapter  extends RecyclerView.Adapter<recycler_image_adapter.MyViewHolder> {
 
     private LayoutInflater inflater;
-    private Context ctx;
+    private Activity ctx;
     private ArrayList<item_image> image_List;
 
-    public recycler_image_adapter(Context ctx , ArrayList<item_image> image_List) {
+    public recycler_image_adapter(Activity ctx, ArrayList<item_image> image_List) {
 
         inflater = LayoutInflater.from(ctx);
         this.ctx = ctx;
         this.image_List = image_List;
-      }
+    }
 
     @Override
     public recycler_image_adapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -37,47 +44,72 @@ public class recycler_image_adapter  extends RecyclerView.Adapter<recycler_image
 
     @Override
     public void onBindViewHolder(final recycler_image_adapter.MyViewHolder holder, int position) {
-        item_image item_obj = image_List.get(position);
+       /* if (position % 2 != 0)
+            holder.getView().setAnimation(AnimationUtils.loadAnimation(ctx, R.anim.left_to_right));
+        else {
+            holder.getView().setAnimation(AnimationUtils.loadAnimation(ctx, R.anim.right_to_left));
 
-        holder.image_view.setImageDrawable(item_obj.getImage());
+        }*/
+        item_image item_obj = image_List.get(position);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        ctx.getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+        holder.image_view.getLayoutParams().width = width / 2;
+        holder.image_view.getLayoutParams().height = width / 2;
+        final int sdk = android.os.Build.VERSION.SDK_INT;
+        if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            holder.image_view.setBackgroundDrawable(ContextCompat.getDrawable(ctx, item_obj.getImage()));
+        } else {
+            holder.image_view.setBackground(ContextCompat.getDrawable(ctx, item_obj.getImage()));
+        }
 
 
     }
+
 
     @Override
     public int getItemCount() {
         return image_List.size();
     }
 
-    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-
+    class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        View view;
         ImageView image_view;
         private CardView card_view;
+
         public MyViewHolder(View itemView) {
             super(itemView);
-
+            view = itemView;
             image_view = (ImageView) itemView.findViewById(R.id.iv_item_image);
-            image_view.setOnClickListener(this);
+            card_view = (CardView) itemView.findViewById(R.id.card_view);
+            card_view.setOnClickListener(this);
         }
 
         // onClick Listener for view
         @Override
         public void onClick(View v) {
-           int radio_position = getPosition();
+            int radio_position = getPosition();
             notifyDataSetChanged();
-            switch (v.getId()){
+            switch (v.getId()) {
 
-                case R.id.iv_item_image : // code
-
-
+                case R.id.card_view: // code
+                    Intent intent = new Intent(ctx, image_activity.class);
+                    intent.putExtra("res", image_List.get(radio_position).getImage());
+                    ctx.startActivity(intent);
+                    new static_tools(ctx).overRideAnimationClose();
+                    
             }
 
         }
 
 
+        public View getView() {
+            return view;
         }
     }
 
 
+}
 
 
--- src/main/java/tech/wallpaperandwallaper/nightwallpaper4k/welcome.java
+++ src/main/java/tech/wallpaperandwallaper/nightwallpaper4k/welcome.java
@@ -3,14 +3,46 @@ package tech.wallpaperandwallaper.nightwallpaper4k;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import tech.wallpaperandwallaper.nightwallpaper4k.adapter.recycler_image_adapter;
+
+import static tech.wallpaperandwallaper.nightwallpaper4k.databases.db.getImageArrayList;
 
 public class welcome extends Activity {
+    RecyclerView myRecycler;
+
     @Override
     protected void onCreate( Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_layout);
+        initView();
+        initAdapter();
 
+    }
+
+    private void initAdapter() {
+
+         recycler_image_adapter adapter = new recycler_image_adapter(this ,getImageArrayList());
+        myRecycler.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
+        myRecycler.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.HORIZONTAL));
+        myRecycler.setItemAnimator(new DefaultItemAnimator());
+        myRecycler.setLayoutManager(new GridLayoutManager(this,2 ));
+
+        myRecycler.setAdapter(adapter);
+
+//        myRecycler.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+    }
+
+    private void initView() {
+
+    myRecycler = (RecyclerView) findViewById(R.id.recycler);
     }
 
 }
