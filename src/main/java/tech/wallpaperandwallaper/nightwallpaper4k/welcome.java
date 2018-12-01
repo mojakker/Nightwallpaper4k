a2ee7f25b1ae70efbdfbf000827b21a394b780c7
MOJAKKER <mojakkerhossain3@gmail.com>
2018-12-01 20:08:47 +0530

    nbsfb vlj

---
+++ src/main/java/tech/wallpaperandwallaper/nightwallpaper4k/welcome.java
@@ -0,0 +1,16 @@
+package tech.wallpaperandwallaper.nightwallpaper4k;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class welcome extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+}
